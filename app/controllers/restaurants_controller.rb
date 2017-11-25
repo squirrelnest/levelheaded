eb26b40bef4944fb7cf6222e86439628a663ef0f
mikka <mikkanthrope@gmail.com>
2017-11-25 01:31:30 -0800

    Added show links to chowtables index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,42 +5,42 @@ class RestaurantsController < ApplicationController
     erb :'/restaurants/index'
   end
 
-  get '/restaurants/new' do
-    erb :'/restaurants/new'
-  end
-
-  post '/restaurants/create' do
-    @restaurant = Restaurant.create(restaurant_params)
-    @restaurant.save
-    redirect "/restaurants/#{@restaurant.id}"
-  end
+  # get '/restaurants/new' do
+  #   erb :'/restaurants/new'
+  # end
+  #
+  # post '/restaurants/create' do
+  #   @restaurant = Restaurant.create(restaurant_params)
+  #   @restaurant.save
+  #   redirect "/restaurants/#{@restaurant.id}"
+  # end
 
   get '/restaurants/:id' do
     @restaurant = Restaurant.find(params[:id])
     erb :'/restaurants/show'
   end
 
-  get '/restaurants/:id/edit' do
-    @restaurant = Restaurant.find(params[:id])
-    erb :'/restaurants/edit'
-  end
+  # get '/restaurants/:id/edit' do
+  #   @restaurant = Restaurant.find(params[:id])
+  #   erb :'/restaurants/edit'
+  # end
 
-  patch '/restaurants/:id' do
-    @restaurant = Restaurant.find(params[:id])
-    @restaurant.update(restaurant_params)
-    redirect "/restaurants/#{@restaurant.id}"
-  end
+  # patch '/restaurants/:id' do
+  #   @restaurant = Restaurant.find(params[:id])
+  #   @restaurant.update(restaurant_params)
+  #   redirect "/restaurants/#{@restaurant.id}"
+  # end
 
   get '/restaurants/:id/delete' do
     @restaurant = Restaurant.find(params[:id])
-    @restaurant.delete_all
+    @restaurant.delete
     redirect "/restaurants"
   end
 
   private
 
   def restaurant_params
-    {name: params[:restaurant][:name], year_completed: params[:restaurant][:year_completed]}
+    {name: params[:restaurant][:name], address_hash: params[:address], phone: params[:phone]}
   end
 
 end
