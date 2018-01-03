7d3c1de3457499101f7f7c04042dd25ab326cac1
mikka <mikkanthrope@gmail.com>
2018-01-03 14:44:26 -0800

    Removed ability to add table without a review

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# levelheaded
+# What is Levelheaded
 Review site of restaurant tables around the world. How wobbly was that table? Mobile app with wobble-meter coming soon.
+
+# Installation
+Fork this repo
+
+# Contributing
+
+# License
--- app/controllers/chowtables_controller.rb
+++ app/controllers/chowtables_controller.rb
@@ -5,35 +5,10 @@ class ChowtablesController < ApplicationController
     erb :'/chowtables/index'
   end
 
-  get '/chowtables/new' do
-    erb :'/chowtables/new'
-  end
-
   get '/chowtables/:id/delete' do
     @chowtable = Chowtable.find(params[:id])
     @chowtable.delete
     redirect '/chowtables'
-  end
-
-  post '/chowtables/create' do
-    @chowtable = Chowtable.create(chowtable_params)
-    # save or create new title section
-    if params[:chowtable][:title_ids]
-      params[:chowtable][:title_ids].each do |title_id|
-        @chowtable.titles << Title.find(title_id)
-      end
-    end
-    @chowtable.titles << Title.find_or_create_by!(name: params[:title][:name]) unless params[:title][:name].empty?
-    # save or create new landmark section
-    if params[:chowtable][:landmark_ids]
-      params[:chowtable][:landmark_ids].each do |landmark_id|
-        @chowtable.landmarks << Landmark.find(landmark_id)
-      end
-    end
-    @chowtable.landmarks << Landmark.find_or_create_by!(name: params[:landmark][:name]) unless params[:landmark][:name].empty?
-    # save and redirect
-    @chowtable.save
-    redirect :'/chowtables'
   end
 
   get '/chowtables/:id' do
@@ -41,38 +16,10 @@ class ChowtablesController < ApplicationController
     erb :'/chowtables/show'
   end
 
-  get '/chowtables/:id/edit' do
-    @chowtable = Chowtable.find(params[:id])
-    erb :'/chowtables/edit'
-  end
-
-  patch '/chowtables/:id' do
-    @chowtable = Chowtable.find(params[:id])
-    @chowtable = @chowtable.update(chowtable_params)
-    # save or create new title section
-    if params[:chowtable][:title_ids]
-      params[:chowtable][:title_ids].each do |title_id|
-        @chowtable.titles << Title.find(title_id)
-      end
-    end
-    @chowtable.titles << Title.find_or_create_by!(name: params[:title][:name]) unless params[:title][:name].empty?
-    # save or create new landmark section
-    if params[:chowtable][:landmark_ids]
-      params[:chowtable][:landmark_ids].each do |landmark_id|
-        @chowtable.landmarks << Landmark.find(landmark_id)
-      end
-    end
-    @chowtable.landmarks << Landmark.find_or_create_by!(name: params[:landmark][:name]) unless params[:landmark][:name].empty?
-    # save and redirect
-    @chowtable.save
-    redirect :"/chowtables/#{@chowtable.id}"
-  end
-
   private
 
   def chowtable_params
     {name: params[:chowtable][:name]}
-    # {name: params[:chowtable][:name], titles: params[chowtable][title_ids][], landmarks: params[chowtable][landmark_ids][]}
   end
 
 end
