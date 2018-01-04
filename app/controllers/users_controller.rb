26b890ea72f2e3030c9a2655daabff3950eb375b
mikka <mikkanthrope@gmail.com>
2018-01-03 16:46:29 -0800

    Added signup and login routes and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,52 @@
 class UsersController < ApplicationController
 
-  get '/users' do
-    @users = User.all
-    erb :'/users/index'
+  get '/users/home' do
+    erb :'/users/home'
   end
 
-  get '/users/new' do
-    erb :'/users/new'
+  get '/users/signup' do
+    erb :'/users/signup'
   end
 
-  post '/users/create' do
-    @user = User.create(user_params)
-    @user.save
-    redirect "/users/#{@user.id}"
+  get '/users/login' do
+    erb :'/users/login'
   end
 
-  get '/users/:id' do
-    @user = User.find(params[:id])
-    erb :'/users/show'
-  end
-
-  get '/users/:id/edit' do
-    @user = User.find(params[:id])
-    erb :'/users/edit'
-  end
-
-  patch '/users/:id' do
-    @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect "/users/#{@user.id}"
-  end
+  # get '/users' do
+  #   @users = User.all
+  #   erb :'/users/index'
+  # end
+  #
+  # get '/users/new' do
+  #   erb :'/users/new'
+  # end
+  #
+  # post '/users/create' do
+  #   @user = User.create(user_params)
+  #   @user.save
+  #   redirect "/users/#{@user.id}"
+  # end
+  #
+  # get '/users/:id' do
+  #   @user = User.find(params[:id])
+  #   erb :'/users/show'
+  # end
+  #
+  # get '/users/:id/edit' do
+  #   @user = User.find(params[:id])
+  #   erb :'/users/edit'
+  # end
+  #
+  # patch '/users/:id' do
+  #   @user = User.find(params[:id])
+  #   @user.update(user_params)
+  #   redirect "/users/#{@user.id}"
+  # end
 
   private
 
   def user_params
-    {name: params[:user][:name], year_completed: params[:user][:year_completed]}
+    {name: params[:user][:name]}
   end
 
 end
